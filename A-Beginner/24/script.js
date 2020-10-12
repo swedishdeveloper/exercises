4aae060a2bd049e45e86f6cc5ac49b3de10f318f
unknown <benjaminprogrammering@gmail.com>
2020-10-12 15:49:45 +0200

    Solve 24

---
+++ A-Beginner/24/script.js
@@ -0,0 +1,11 @@
+console.log(isAllCaps("TEST")); //returns true
+console.log(isAllCaps("Test")); //returns false
+
+function isAllCaps(text) {
+    for (let i = 0; i < text.length; i++) {
+        if (text.charAt(i) != text.charAt(i).toUpperCase()) {
+            return false;
+        }
+    }
+    return true;
+}
